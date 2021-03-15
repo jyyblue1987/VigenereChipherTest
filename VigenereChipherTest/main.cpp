44b25edd54c1defe66ae3a2f3025d8923f39c03a
jyyblue <jyyblue1987@outlook.com>
2021-03-16 04:58:09 +0800

    [add] add option2.

--- VigenereChipherTest/main.cpp
+++ VigenereChipherTest/main.cpp
@@ -28,10 +28,27 @@ using namespace std;
 string vigenereCipher(const char *key, const char *plain);
 string vigenereDecipher(const char *key, const char *cipher);
 
+int checkValidSecret(const char *key) {
+	int len1 = strlen(key);
+	for(int i = 0; i < len1; i++)
+	{
+		char k = key[i];
+		if( 'a' <= k && k <= 'z' || 'A' <= k && k <= 'Z' )
+		{
+		}
+		else
+			return 0;
+	}
+
+	return 1;
+}
+
 int main(){
 	string choice = "";
 	string message = "";
 	string secret = "";
+	string cipher = "";
+	string decrpyted = "";
 
     while(true)
 	{
@@ -47,25 +64,65 @@ int main(){
 		{
 			cout << "Enter the message (3 + characters): ";			
 			getline(cin, message);
-			cout << "Enter the secret (empty or English letters): ";
-			getline(cin, secret);
 
 			message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
+			if( message.size() < 3 )
+			{
+				cout << "The message is too short!" << endl << endl;
+				break;
+			}
+
+			cout << "Enter the secret (empty or English letters): ";
+			getline(cin, secret);
 			secret.erase(std::remove(secret.begin(), secret.end(), '\n'), secret.end());
+
+			int flag = checkValidSecret(secret.c_str());
+			if( flag == 0 )
+			{
+				cout << "The secret must be empty or should have only English letters [a-z] and [A-Z]!" << endl << endl;
+				break;
+			}
 
 			cout << endl;
 			cout << "Original message: " << message << endl;
 			cout << "The secret:       " << secret << endl;
 
-			string cipher = vigenereCipher(secret.c_str(), message.c_str());
+			cipher = vigenereCipher(secret.c_str(), message.c_str());
 			cout << "Encrypted text:   " << cipher << endl;
 
-			string decrpyted = vigenereDecipher(secret.c_str(), cipher.c_str());
-			cout << "Decrypted text:   " << decrpyted << endl;
+			decrpyted = vigenereDecipher(secret.c_str(), cipher.c_str());
+			cout << "Decrypted text:   " << decrpyted << endl << endl;
 
 		}
 		else if( choice == "2" )
 		{
+			cout << "Enter the encrypted text (3 + characters): ";			
+			getline(cin, message);
+
+			message.erase(std::remove(message.begin(), message.end(), '\n'), message.end());
+			if( message.size() < 3 )
+			{
+				cout << "The message is too short!" << endl << endl;
+				break;
+			}
+
+			cout << "Enter the secret (empty or English letters): ";
+			getline(cin, secret);
+			secret.erase(std::remove(secret.begin(), secret.end(), '\n'), secret.end());
+
+			int flag = checkValidSecret(secret.c_str());
+			if( flag == 0 )
+			{
+				cout << "The secret must be empty or should have only English letters [a-z] and [A-Z]!" << endl << endl;
+				break;
+			}
+
+			cout << endl;
+			cout << "Encrypted text: " << message << endl;
+			cout << "The secret:       " << secret << endl;
+
+			string decrpyted = vigenereDecipher(secret.c_str(), message.c_str());
+			cout << "Decrypted text:   " << decrpyted << endl << endl;
 		}
 		else
 			break;
